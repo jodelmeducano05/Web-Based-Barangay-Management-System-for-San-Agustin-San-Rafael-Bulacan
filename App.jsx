32ff3ae7e1e24fe6c974a3eed8e00cdd10f17bac
jodelmeducano05 <jodelmeducano@gmail.com>
2026-09-10 12:39:12 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -686,7 +686,7 @@ function Sidebar({ role, page, setPage, logout }) {
       "Announcements",
       "Blotter Reports",
     ];
-  } else {role=== "Resident") {
+  } else if {role=== "Resident") {
     menu = [
       "Dashboard",
       "My Requests",
